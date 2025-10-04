6e8110c1fe5f7662a76ef5de1eb2e37fd9b62ad7
Carlos Costa <carllos.nc@gmail.com>
2025-10-04 20:45:51 -0300

    fix: update ButtonExample layout and improve spacing for better presentation

--- src/examples/button-example.tsx
+++ src/examples/button-example.tsx
@@ -3,15 +3,24 @@ import { Button } from "../components/buttons";
 
 export function ButtonExample(){
   return (
-    <div className="p-[20px] inline-flex flex-col gap-[20px]">
-      <h3 className="text-neutral-800 font-bold text-[20px]">BUTTONS</h3>
+    <div className="p-[20px] flex flex-col gap-[30px]">
+      <h3 className="text-neutral-800 font-bold text-[20px]">Buttons</h3>
 
-      <div className="flex gap-[10px]">
-        <Button> Hello world </Button>
+      <div className="flex flex-wrap gap-[10px]">
+        <Button> Button </Button>
         <Button isLoading> Loading </Button>
         <Button leftIcon={(<MdStar />)}>Button</Button>
         <Button rightIcon={(<MdStar />)}>Button</Button>
         <Button leftIcon={(<MdStar />)} rightIcon={(<MdStar />)}>Button</Button>
+      </div>
+
+      <div className="flex gap-[20px]">
+        <a target="_blank" href="https://raw.githubusercontent.com/carllosnc/comps/refs/heads/master/src/components/buttons.tsx">
+          Code
+        </a>
+        <a target="_blank" href="https://github.com/carllosnc/comps/blob/master/src/docs/button.md">
+          Documentation
+        </a>
       </div>
     </div>
   )
